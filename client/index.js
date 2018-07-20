b03d29db6ba865ce5deef4f6a9d36bdce2cb7d9c
Matt Dworkin <mdworkin21@gmail.com>
2018-07-20 14:44:20 -0400

    moreBoiler

--- client/index.js
+++ client/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import {Provider} from 'react-redux'
 import store from './store'
+import style from '../public/style.css'
 
 ReactDom.render(
     <Provider store={store}>
