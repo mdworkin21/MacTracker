f244050dfd12390991b135a31d6998276806e943
Matt Dworkin <mdworkin21@gmail.com>
2018-08-06 11:17:21 -0400

    env variables

--- server/db/database.js
+++ server/db/database.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize')
-const db = new Sequelize('postgres://localhost:5432/MacTrack', {logging: false})
+const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/MacTrack', {logging: false})
 
 //This file creates the db connectiomn with postgres. Models are defined in their own folders.
 
